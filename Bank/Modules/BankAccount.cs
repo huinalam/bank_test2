3b9e4ca6d628e031fcd2168a9b7d3c4ebd75b93f
huinalam <huinalam@gmail.com>
2016-12-16 07:07:54 +0900

    init

---
+++ Bank/Modules/BankAccount.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Modules
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        {
+            
+        }
+
+        /// <summary>
+        /// 잔고
+        /// </summary>
+        public int Balance { get; private set; }
+
+        /// <summary>
+        /// 출금
+        /// </summary>
+        /// <param name="money">금액</param>
+        public void Withdraw(int amount)
+        {
+            if (Balance > amount)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+            Balance -= amount;
+        }
+
+        /// <summary>
+        /// 입금
+        /// </summary>
+        /// <param name="amount">입금 금액</param>
+        public void Deposit(int amount)
+        {
+            Balance += amount;
+        }
+    }
+}
